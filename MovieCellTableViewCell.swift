6c6f6b0e7c48b03e339fd4222b8569c95e177f09
Megha Ghosh <70420648+ahgem2@users.noreply.github.com>
2022-09-13 01:19:13 -0500

    Add files via upload

---
+++ MovieCellTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MovieCellTableViewCell.swift
+//  Gem
+//
+//  Created by Megha Ghosh on 9/12/22.
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
